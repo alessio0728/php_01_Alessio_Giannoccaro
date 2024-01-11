1e94feac3717da8f8fdb95cf63af41f6e067ba10
alessio giannoccaro <mrbigalessio@gmailcom>
2024-01-11 01:48:55 +0100

    salva1

---
+++ php1.php
@@ -0,0 +1,31 @@
+<?php
+
+// Variabile numero intero
+$numero_intero = 42;
+
+// Variabile Float
+$numero_float = 3.14;
+
+// Variabile Stringa
+$stringa = "Prima prova con PHP !!!"; //Risultato frase copiata con i simboli
+
+$stringaDiversa = "";
+
+// Variabile Boolean
+$booleano = true; // risultato vero
+
+$booleanoF = false; // risultato falso
+
+//echo$numero_intero;
+
+//echo$numero_float;
+
+//echo$numero_stringa;
+
+//echo$booleano;
+
+//echo$booleanoF;
+
+
+// Puoi anche usare var_dump per visualizzare il tipo e il valore della variabile
+var_dump($numero_intero, $numero_float, $stringa, $booleano, $booleanoF);
